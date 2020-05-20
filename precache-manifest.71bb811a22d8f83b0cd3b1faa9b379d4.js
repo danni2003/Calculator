01469da29d0d6bb8a5b888817d96f41cc423387e
someone <you@example.com>
2020-05-20 22:41:57 +0000

    Updates

---
+++ precache-manifest.71bb811a22d8f83b0cd3b1faa9b379d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48fb112471f8f08212a29f83c2c7fdf9",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "2eeb1a8136405c8aa79b",
+    "url": "/Calculator/static/css/main.555a4310.chunk.css"
+  },
+  {
+    "revision": "2dc30cb1d17d497127aa",
+    "url": "/Calculator/static/js/2.f848b2a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculator/static/js/2.f848b2a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2eeb1a8136405c8aa79b",
+    "url": "/Calculator/static/js/main.4b6fdd04.chunk.js"
+  },
+  {
+    "revision": "4114f83e4f86995ba143",
+    "url": "/Calculator/static/js/runtime-main.36230641.js"
+  }
+]);
